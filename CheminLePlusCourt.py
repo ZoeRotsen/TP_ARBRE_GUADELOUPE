87dc777e0030db255b30c83083e146c8511dfe98
ZoeRotsen <zoe.rotsenpro@gmail.com>
2024-01-29 15:35:29 -0400

    V1.0

--- CheminLePlusCourt.py
+++ CheminLePlusCourt.py
@@ -10,7 +10,7 @@ G=nx.from_pandas_edgelist(data,source="De",target="À",edge_attr="Distance")
 
 #Création du tableau des choix des communes possibles
 communes=["Abymes","Anse-Bertrand","Baie-Mahault","Baillif","Bouillante","Capesterre","Deshaies","Gosier","Gourbeyre","Goyave","Lamentin","Morne à l eau","Le Moule","Petit  Bourg","Petit Canal","Port-Louis","Pointe-Noire","Pointe à Pitre","Sainte-Anne","Saint-Claude","Saint-François","Saint-Rose","Trois-Rivières","Vieux-Fort","Vieux-Habitants"]
-print("Listes des communes :\n 0-Abymes \n 1-Anse-Bertrand \n 2-Baie-Mahault \n 3-Baillif \n 4-Bouillante \n 5-Capesterre \n 6-Deshaies \n 7-Gosier \n 8-Gourbeyre \n 9-Goyave \n 10-Lamentin \n 11-Morne à l'eau \n 12-Le Moule \n 13-Petit  Bourg \n 14-Petit Canal \n 15-Port-Louis \n 16-Pointe-Noire \n 17-Pointe à Pitre \n 18-Sainte-Anne \n 19-Saint-Claude \n 20-Saint-François\n 21-Saint-Rose\n 22-Trois-Rivières \n 23-Vieux-Fort \n 24-Vieux-Habitants")
+print("Listes des communes :\n 0-Abymes \n 1-Anse-Bertrand \n 2-Baie-Mahault \n 3-Baillif \n 4-Bouillante \n 5-Capesterre \n 6-Deshaies \n 7-Gosier \n 8-Gourbeyre \n 9-Goyave \n 10-Lamentin \n 11-Morne à l eau \n 12-Le Moule \n 13-Petit  Bourg \n 14-Petit Canal \n 15-Port-Louis \n 16-Pointe-Noire \n 17-Pointe à Pitre \n 18-Sainte-Anne \n 19-Saint-Claude \n 20-Saint-François\n 21-Saint-Rose\n 22-Trois-Rivières \n 23-Vieux-Fort \n 24-Vieux-Habitants")
 
 depart=int(input("Saissisez la commune de départ en choisissant le numéro correspondant :"))
 pointsLivraison=[]
@@ -29,7 +29,7 @@ while response==True and i<5:
         i=i+1
     else:
         response=False
-        
+
 #On cherche le cycle qui contient tous les points de livraison et commencant au point de depart
 trajet=nx.approximation.traveling_salesman_problem(G,weight="Distance",nodes=pointsLivraison,cycle=True)
 
